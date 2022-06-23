cf0867d187d384052b7cd9d563f65dc0da55d1dc
samuelowino <samuelowino43@yahoo.com>
2022-06-24 02:59:09 +0300

    Added new locale codes

--- src/main/java/com/owino/mobiletranslate/common/LanguagesDirectory.java
+++ src/main/java/com/owino/mobiletranslate/common/LanguagesDirectory.java
@@ -10,14 +10,17 @@ public class LanguagesDirectory {
 
     public static final Logger LOGGER = Logger.getLogger(TranslateApplication.class.getSimpleName());
 
-    private final List<String> languages = Arrays.asList(
-            "ha", "pl", "am", "ar", "az", "be", "bg", "bn", "bs", "fr", "ga",
-            "ca", "cs", "da", "es", "et", "fa", "fi", "gl",
-            "it", "iw", "ja", "jv", "ka", "km", "ko", "ky",
-            "my", "nb", "ne", "nl", "nn", "no", "pa", "ro", "ru", "si", "sk", "sl", "sq", "sr",
-            "sv", "sw", "ta", "te", "th", "tr", "vi", "zh", "zu", "kn", "kk",
-            "af", "de", "fil", "hi", "hr", "hu", "hy", "id", "in", "lo", "lv", "mk", "ml", "mn", "pt", "uk", "ur", "ms", "is", "el", "mr"
-    );
+     private final List<String> languages = Arrays.asList(
+             "ha", "pl", "am", "ar", "az", "be", "bg", "bn", "bs", "fr", "ga",
+             "ca", "cs", "da", "es", "et", "fa", "fi", "gl",
+             "it", "iw", "ja", "jv", "ka", "km", "ko", "ky",
+             "my", "nb", "ne", "nl", "nn", "no", "pa", "ro", "ru", "si", "sk", "sl", "sq", "sr",
+             "sv", "sw", "ta", "te", "th", "tr", "vi", "zh", "zu", "kn", "kk",
+             "af", "de", "fil", "hi", "hr", "hu", "hy", "id", "in", "lo", "lv", "mk", "ml",
+             "mn", "pt", "uk", "ur", "ms", "is", "el", "mr",
+             "haw","he","ku","la","lt","lb","so"
+     );
+
 
     public List<String> findUniqueListOfLocales() {
 
