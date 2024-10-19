48dbfe270bbc136eeab4242de9513232660784b9
mwangidennis1 <mwangedennis05@gmail.com>
2024-10-19 08:02:05 +0300

    add sqlite driver plus database startup script for testing

--- src/main/java/com/owino/mobiletranslate/rest/Main.java
+++ src/main/java/com/owino/mobiletranslate/rest/Main.java
@@ -1,7 +1,9 @@
 package com.owino.mobiletranslate.rest;
 
 public class Main {
+
     public static void main(String[] args) {
+        Database.initializeDatabase();
         //System.out.println("Hello World");
     }
 }
